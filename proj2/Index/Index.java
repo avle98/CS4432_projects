05508b37f6f64a662a55a35e5d3bc7ce67e9ed86
Ann <avle9721@gmail.com>
2020-04-23 18:09:41 -0400

    delete

--- proj2/Index/Index.java
+++
@@ -1,9 +0,0 @@
-package Index;
-import Block.Record;
-import java.util.ArrayList;
-
-public interface Index {
-    ArrayList<Record> get(Integer value);
-    void put(Integer value, IndexPointer indexPointer);
-    String toString();
-}
